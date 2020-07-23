3a8e37da9909f4b491f310af77f34d805a0e3a87
Catherine <catherineep4189@gmail.com>
2020-07-23 19:31:00 +0100

    added criteria via input prompt

--- Recipe.py
+++ Recipe.py
@@ -5,8 +5,8 @@ id = hiddenkeys.app_id
 key = hiddenkeys.app_key
 
 
-def recipe_database(ingredient):
-    url = f'https://api.edamam.com/search?q={ingredient}&app_id={id}&app_key={key}'
+def recipe_database(ingredient, health_labels):
+    url = f'https://api.edamam.com/search?q={ingredient}&app_id={id}&app_key={key}&Health={health_labels}'
 
     response = requests.get(url)
     found_recipes = response.json()
@@ -15,8 +15,9 @@ def recipe_database(ingredient):
 
 def recipe_search():
     ingredient_criteria = input('What ingredient would you like to include in the recipe? ')
+    health_criteria = input('Do you have any specific dietary and/or allergy-free requests? ')
 
-    answer = recipe_database(ingredient_criteria)
+    answer = recipe_database(ingredient_criteria, health_criteria)
 
     print(answer)
 
