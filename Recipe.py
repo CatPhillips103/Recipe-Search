7abd983d615fb85cb7d69f31d6c90e4e83b5e83c
Catherine <catherineep4189@gmail.com>
2020-07-23 15:54:11 +0100

    Input used for prompt question - when responded to the hits found within API data should return and print recipe to terminal

--- Recipe.py
+++ Recipe.py
@@ -4,13 +4,21 @@ import hiddenkeys
 id = hiddenkeys.app_id
 key = hiddenkeys.app_key
 
+
 def recipe_database(ingredient):
     url = f'https://api.edamam.com/search?q={ingredient}&app_id={id}&app_key={key}'
 
     response = requests.get(url)
     found_recipes = response.json()
-    print(found_recipes["hits"])
-
-recipe_database('sumac')
+    return found_recipes["hits"]
 
 
+def recipe_search():
+    ingredient_criteria = input('What ingredient would you like to include in the recipe? ')
+
+    answer = recipe_database(ingredient_criteria)
+
+    print(answer)
+
+
+recipe_search()
